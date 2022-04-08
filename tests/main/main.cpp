b1d001e58f76879310923842e6a483f9dae0226b
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-04-08 19:48:56 +0900

    Add test

--- tests/main/main.cpp
+++ tests/main/main.cpp
@@ -1,11 +1,30 @@
 #define CATCH_CONFIG_MAIN
-#include <algorithm>
 #include <catch2/catch.hpp>
 
-TEST_CASE("test name", "[label1][label2]") {}
+#include <string_view>
+#include <unordered_map>
+#include <tpl/string_template.hpp>
 
-TEMPLATE_TEST_CASE("algorithm equal", "[algorithm][equal]", (std::array<int, 3>),
-                   std::forward_list<int>, std::list<int>, std::vector<int>, int[]) {
-  TestType a{1, 2, 3};
-  CHECK(std::ranges::equal(a, a));
+TEST_CASE("main", "[main]") {
+  {
+    std::string_view s = "abc123def";
+    const std::regex re{R"(\d+)"};
+    constexpr auto fn = [](auto&&) -> std::string_view { return "example"; };
+    CHECK(tpl::regex_replace_fn(s, re, fn) == "abcexampledef");
+  }
+  {
+    std::unordered_map<std::string_view, std::string_view> map{
+      {"what", "example"},
+    };
+    std::string_view s1 = "This is $what.";
+    std::string_view s2 = "This is ${what}ified.";
+    std::string_view s3 = "This is dollar →$$←.";
+    std::string_view s4 = "This is error →$←.";
+    std::string_view s5 = "This is none →$which←.";
+    CHECK(tpl::substitute(s1, map) == "This is example.");
+    CHECK(tpl::substitute(s2, map) == "This is exampleified.");
+    CHECK(tpl::substitute(s3, map) == "This is dollar →$←.");
+    CHECK(tpl::substitute(s4, map) == "This is error →ERROR←.");
+    CHECK(tpl::substitute(s5, map) == "This is none →NONE←.");
+  }
 }
