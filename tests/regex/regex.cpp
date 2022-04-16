5d1759d820c649d27a91e566ca65dfa64ad62d0a
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-04-16 18:03:59 +0900

    ✅ Add test for regex_range

--- tests/regex/regex.cpp
+++ tests/regex/regex.cpp
@@ -1,12 +1,21 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
+#include <ranges>
 #include <string_view>
 #include <unordered_map>
 #include <strtpl/regex.hpp>
 namespace regex = strtpl::regex;
 
 TEST_CASE("regex", "[regex]") {
+  {
+    std::string_view s = "abc123def123ghi";
+    const std::regex re{R"(\d+)"};
+    std::ranges::common_range auto r = regex::regex_range(s, re);
+    for (const auto& mr : r) {
+      CHECK(mr.format("$0") == "123");
+    }
+  }
   {
     std::string_view s = "abc123def";
     const std::regex re{R"(\d+)"};
