23db711ae3e90b4dcce22e9f03e0afef8c874a0c
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-06-22 10:51:38 +0900

    ✅ Update tests

--- tests/trailing_view/trailing_view.cpp
+++ tests/trailing_view/trailing_view.cpp
@@ -1,6 +1,5 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
-
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
 #include <algorithm> // std::ranges::equal
 #include <array>
 #include <vector>
