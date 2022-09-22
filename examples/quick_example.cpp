5401e866d61682b221bb09f37f27cac1d96c5ed9
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-09-22 16:07:08 +0900

    Small fix

--- examples/quick_example.cpp
+++ examples/quick_example.cpp
@@ -3,32 +3,35 @@
 #include <unordered_map>
 #include <strtpl/string_template.hpp>
 
+void substitute_test(std::string_view str,
+                     const std::unordered_map<std::string_view, std::string_view>& map) {
+  try {
+    std::cout << strtpl::substitute(str, map) << std::endl;
+  } catch (const std::exception& e) {
+    std::cout << e.what() << std::endl;
+  }
+}
+
 int main() {
   std::unordered_map<std::string_view, std::string_view> map{
     {"who", "Alice"},
-    {"what", "bananas"},
+    {"what", "banana"},
     {"verb", "run"},
   };
-  { // '$' に続く単語を発見した場合、その単語を map の key として得られる値で置き換えます。
-    std::cout << strtpl::substitute("$who likes $what.", map) << std::endl;
-    // → Alice likes bananas.
-  }
-  { // '$' と単語の後にアルファベットが続く場合は、単語を "{}" で囲みます。
-    std::cout << strtpl::substitute("$who ${verb}s fast.", map) << std::endl;
-    // → Alice runs fast.
-  }
+
+  // '$' に続く単語を発見した場合、その単語を map の key として得られる値で置き換えます。
+  substitute_test("$who likes $what.", map);
+  // output: Alice likes banana.
+
+  // '$' と単語の後にアルファベットが続く場合は、単語を "{}" で囲みます。
+  substitute_test("$who ${verb}s fast.", map);
+  // output: Alice runs fast.
+
   // '$' に続く単語が map に含まれない場合、例外を送出します。
-  try {
-    std::cout << strtpl::substitute("$who likes $where.", map) << std::endl;
-  } catch (const std::exception& e) {
-    std::cout << e.what() << std::endl;
-    // → strtpl::at: index out of range
-  }
+  substitute_test("$who likes $where.", map);
+  // output: Error: key not found
+
   // '$' に続く文字が無効な場合も、例外を送出します。エラーメッセージにはエラーが生じた文字の行番号と列番号を含みます。
-  try {
-    std::cout << strtpl::substitute("$who likes $.", map) << std::endl;
-  } catch (const std::exception& e) {
-    std::cout << e.what() << std::endl;
-    // → Invalid placeholder in string: line 1, col 12
-  }
+  substitute_test("$who likes $.", map);
+  // output: Invalid placeholder in string: line 1, col 12
 }
